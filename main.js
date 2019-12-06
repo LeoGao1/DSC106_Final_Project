d6f1e8753330b364bb9522ebfc168c59f7c0da9a
Leo <weg021@ucsd.edu>
2019-12-06 15:28:24 -0800

    modified:   main.js

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 
 
 Highcharts.ajax({
-    url: 'https://raw.githubusercontent.com/LeoGao1/DSC106_Final_Project/master/final.csv',
+    url: 'https://raw.githubusercontent.com/LeoGao1/DSC106_Final_Project/master/final.json',
     dataType: 'text',
     success: function (activity) {
 
         // transfer the data into different type
         activity = JSON.parse(activity);
+        console.log(activity[0]);
 
 
   }});
